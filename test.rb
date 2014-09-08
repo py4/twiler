8b17f634fc937354239895ebbbc23a6ff4a2eb2c
Pooya <ibtkm2009@gmail.com>
2014-09-08 18:42:44 +0430

    sdad

--- test.rb
+++ test.rb
@@ -2,7 +2,7 @@ require 'twitter'
 client = Twitter::REST::Client.new do |config|
     config.consumer_key        = "6clMQc5LA3irCE7JSgM3AC0jL"
     config.consumer_secret     = "vDFGq7hrwrUgTfRmvui59dMdABNtGg1lwaN14SMdphvMyBToTv"
-    config.access_token        = "27039781-0mljdsIiAZ0EV6ZqsLJXCOKjuhLrwZ3xFCUx4269"
+    config.access_token        = "327039781-0mljdsIiAZ0EV6ZqsLJXCOKjuhLrwZ3xFCUx4269"
     config.access_token_secret = "r9T1w5ENSGpP9KBnApqTEsaXWe0Ja49TTw20NR4h416GS"
 end
 
