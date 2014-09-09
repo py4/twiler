3510a30517cecc72e7a700dd9f497cd43f601d05
Pooya <ibtkm2009@gmail.com>
2014-09-10 03:09:51 +0430

    df

--- test.rb
+++ test.rb
@@ -6,7 +6,7 @@ client = Twitter::REST::Client.new do |config|
     config.access_token_secret = "r9T1w5ENSGpP9KBnApqTEsaXWe0Ja49TTw20NR4h416GS"
 end
 
-client.search("startup").count #take(3).collect do |tweet|
+puts client.search("startup").count #take(3).collect do |tweet|
     #puts "#{tweet.user.methods}"
     #puts tweet.retweeted_by
     #puts "#{tweet}"
